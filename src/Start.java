4d44d0e330b4b13b256adc44bdec47a098c28a4a
Evgeny Ostrovsky <t-evostr@microsoft.com>
2019-04-20 18:14:23 +0300

    Update

--- src/Start.java
+++ src/Start.java
@@ -1,13 +1,20 @@
 
-import javax.net.ssl.SSLException;
-
 public class Start {
 
-	public static void main(String[] args) throws SSLException {
+	public static void main(String[] args) {
 
-		Connect connect = new Connect();
-		
-		connect.printUsers();
+		import javafx.application.Application;
+		import javafx.scene.Scene;
+		import javafx.stage.Stage;
+		import javafx.scene.layout.StackPane;
+		import javafx.scene.control.Button;
+		import javafx.scene.control.TextField;
+		import javafx.event.ActionEvent;
+		import javafx.event.EventHandler;
+		import javafx.geometry.*;
+
+//		Connect connect = new Connect();
+//		connect.printUsers();
 			
 	}
 	
