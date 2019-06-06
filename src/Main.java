e4e2f69b1a30f99ea34a029ce29172d04b37faaa
YarinGross18 <44318863+YarinGross18@users.noreply.github.com>
2019-06-06 04:50:09 +0300

    6.6 night update

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -16,21 +17,57 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 
 import com.google.gson.Gson;
 
 import Requests.Message;
 import Requests.Register;
 import Requests.Request;
+import Requests.AcountValid;
 
 public class Main extends Application {
+    /****Scenes declare****/
+
 	Stage window;
+	Scene welcomeR;
 	Scene signIn;
+	Scene signInR;
 	Scene signUpS;
 	Scene menu;
 	Scene guestScene;
 	Scene verifyScene;
 	Scene welcome;
+	
+    /****Textfields declare****/
+
+	
+    TextField searchTF=new TextField("Type map to search");
+    TextField name = new TextField("Please enter username");
+    TextField password = new TextField("Please enter password");
+    TextField nameW = new TextField("Please enter username");
+    TextField passwordW = new TextField("Please enter password");
+    TextField command = new TextField("Please enter command");
+    TextField nameR =new TextField("Please enter username");
+    TextField passwordR = new TextField("Please enter password");
+    TextField creditCard = new TextField("Please enter credit card");
+    TextField email= new TextField("Please enter your email addres");
+    TextField verifySerial=new TextField("Please enter the serial we sent to you.");
+    
+    
+    /***Clean all  textfields function***/
+    
+    public  void clean_tf() {
+    	     searchTF.setText("Type map to search");
+    	     name.setText("Please enter username");
+    	     password.setText("Please enter password");
+    	     command.setText("Please enter command");
+    	     nameR.setText("Please enter username");
+    	     passwordR.setText("Please enter password");
+    	     creditCard.setText("Please enter credit card");
+    	     email.setText("Please enter your email addres");
+    	     verifySerial.setText("Please enter the serial we sent to you.");
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -38,6 +75,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+    	    	
     	window=primaryStage;
     	
         TextField helloTF = new TextField("");
@@ -45,20 +83,18 @@ public class Main extends Application {
         primaryStage.setTitle("GCM");
         
        
-        /****Textfields declare****/
-        TextField searchTF=new TextField("Type map to search");
-        TextField name = new TextField("Please enter username");
+        /****Textfields edit****/
+        searchTF.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
         name.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
-
-        TextField password = new TextField("Please enter password");
         password.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
-        TextField command = new TextField("Please enter command");
-        TextField nameR =new TextField("Please enter username");
-        TextField passwordR = new TextField("Please enter password");
-        TextField creditCard = new TextField("Please enter credit card");
-        TextField email= new TextField("Please enter your email addres");
-        TextField verifySerial=new TextField("Please enter the serial we sent to you.");
-        
+        nameW.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+        passwordW.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+        nameR.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+        passwordR.setFont(Font.font("Verdana", FontWeight.BOLD, 12));;
+        creditCard.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+        email.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+        verifySerial.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+
         /****Checkboxs declare ***/
         CheckBox OneTimePruchase= new CheckBox("One time pruchase");
         CheckBox HalfYearPruchase= new CheckBox("But for 6 months");
@@ -66,6 +102,7 @@ public class Main extends Application {
         
         /****Buttons declare****/
         Button next = new Button();
+        Button nextW = new Button();
         Button btn = new Button();
         Button memBtn = new Button();
         Button guestBtn = new Button();
@@ -76,15 +113,23 @@ public class Main extends Application {
         Button getBack2=new Button();
         Button getBack3=new Button();
         Button getBack4=new Button();
-        
+        Button getBack5=new Button();
+        Button getBack6=new Button();
+        Button search_btn=new Button();
+        Button workers_zone=new Button();
         
         /**** Buttons Text ****/
         next.setText("Next");
+        nextW.setText("Next");
+        workers_zone.setText("Workers zone");
+        search_btn.setText("Search");
         signUp.setText("Sign up as our customer");
         signUp2.setText("Sign up");
+        getBack5.setText("Go to Main");
         getBack4.setText("Go back");
         getBack3.setText("Go back");
         getBack2.setText("Go back");
+        getBack6.setText("Go back");
         getBack.setText("Go back");
         search.setText("Search");
         memBtn.setText("I am a member");
@@ -97,22 +142,38 @@ public class Main extends Application {
         
         
         /****Buttons actions****/
+        
+        search_btn.setOnAction(e->{
+        	Gson gson = new Gson();
+        	String question=searchTF.getText();
+        	String jsonString = gson.toJson(question);
+        	chat.SendToServer(jsonString);
+        	
+        });
+        
+        
         signUp2.setOnAction(e->{
-        	window.setScene(verifyScene);
         	Gson gson = new Gson();
         	Register register = new Register(nameR.getText(), passwordR.getText(), email.getText(), creditCard.getText());
         	Request request = new Request(2, register);
         	String jsonString = gson.toJson(request);
         	chat.SendToServer(jsonString);
         	
+        	window.setScene(welcomeR);
+
         });
+        
+        
         signUp.setOnAction(e->window.setScene(signUpS));
         memBtn.setOnAction(e->window.setScene(signIn));
         guestBtn.setOnAction(e->window.setScene(guestScene));
-        getBack.setOnAction(e->window.setScene(menu));
-        getBack2.setOnAction(e->window.setScene(menu));
-        getBack3.setOnAction(e->window.setScene(menu));
-        getBack4.setOnAction(e->window.setScene(menu));
+        getBack.setOnAction(e->{window.setScene(menu);clean_tf();});
+        getBack2.setOnAction(e->{window.setScene(menu);clean_tf();});
+        getBack3.setOnAction(e->{window.setScene(menu);clean_tf();});
+        getBack4.setOnAction(e->{window.setScene(menu);clean_tf();});
+        getBack5.setOnAction(e->{window.setScene(menu);clean_tf();});
+        getBack6.setOnAction(e->{window.setScene(menu);clean_tf();});
+        workers_zone.setOnAction(e->{window.setScene(signInR);clean_tf();});
 
        
         
@@ -127,19 +188,40 @@ public class Main extends Application {
             }
         });
 
+        nextW.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	Gson gson = new Gson();
+            	AcountValid message = new AcountValid(name.getText(), password.getText());
+            	String jsonString = gson.toJson(message);
+            	chat.SendToServer(jsonString);
+            }
+        });
         
+        /****background zone****/
         
         BackgroundImage myBI= new BackgroundImage(new Image("Images\\Background.png"),
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        BackgroundImage myBIwe= new BackgroundImage(new Image("Images\\Welcome.png"),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        BackgroundImage myBIs= new BackgroundImage(new Image("Images\\sign_up.png"),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
         BackgroundImage myBIW= new BackgroundImage(new Image("Images\\signInIm.png"),
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        BackgroundImage myBIWr= new BackgroundImage(new Image("Images\\Wrong.png"),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        BackgroundImage myBIc= new BackgroundImage(new Image("Images\\catalog.png"),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        
         
         /****Scene declare****/
         
         StackPane root = new StackPane();
-
-        root.getChildren().add(memBtn);
+    
         root.setBackground(new Background(myBI));
+        root.getChildren().add(workers_zone);
+        workers_zone.setTranslateY(-100);
+        root.getChildren().add(memBtn);
         memBtn.setTranslateY(-250);
         root.getChildren().add(guestBtn);
         guestBtn.setTranslateY(-200);
@@ -165,42 +247,68 @@ public class Main extends Application {
         
         
         StackPane guestZone = new StackPane();
+        guestZone.setBackground(new Background(myBIc));
+        searchTF.setMaxWidth(300);
         guestZone.getChildren().add(searchTF);
+        guestZone.getChildren().add(search_btn);
         guestZone.getChildren().add(getBack2);
         searchTF.setTranslateY(-100);
-        getBack2.setTranslateY(50);
-        guestScene=new Scene(guestZone,300,300);
+        getBack2.setTranslateX(-100);
+        guestScene=new Scene(guestZone,1280,720);
 
+        
+        StackPane welcomeRs=new StackPane();
+        welcomeRs.setBackground(new Background(myBIwe));
+        welcomeRs.getChildren().add(getBack5);
+        welcomeR=new Scene(welcomeRs,1280,720);
+
+
+        
+        
         StackPane sign_Up = new StackPane();
-        sign_Up.getChildren().add(nameR);
+        sign_Up.setBackground(new Background(myBIs));
         nameR.setTranslateY(-100);
-        sign_Up.getChildren().add(passwordR);
+        nameR.setMaxWidth(300);
+        sign_Up.getChildren().add(nameR);
+        passwordR.setMaxWidth(300);
         passwordR.setTranslateY(-50);
-        sign_Up.getChildren().add(creditCard);
+        sign_Up.getChildren().add(passwordR);
+        creditCard.setMaxWidth(300);
         creditCard.setTranslateY(0);
-        sign_Up.getChildren().add(email);
+        sign_Up.getChildren().add(creditCard);
+        email.setMaxWidth(300);
         email.setTranslateY(50);
+        sign_Up.getChildren().add(email);
         sign_Up.getChildren().add(signUp2);
         signUp2.setTranslateY(100);
         sign_Up.getChildren().add(getBack3);
         getBack3.setTranslateY(100);
         getBack3.setTranslateX(-100);
-        signUpS=new Scene(sign_Up,300,300);
+        signUpS=new Scene(sign_Up,1280,720);
         
         
+        StackPane workersZone = new StackPane();
+        workersZone.setBackground(new Background(myBIW));
+        nameW.setTranslateY(-50);
+        nameW.setMaxWidth(400);
+        workersZone.getChildren().add(nameW);
+        passwordW.setMaxWidth(400);
+        workersZone.getChildren().add(passwordW);
+        workersZone.getChildren().add(getBack6);
+        getBack6.setTranslateY(100);
+        getBack6.setTranslateX(-100);
+        workersZone.getChildren().add(nextW);
+        nextW.setTranslateY(100);
+        signInR=new Scene(workersZone,1280,720);
         
         
         StackPane memberZone = new StackPane();
-        memberZone.setBackground(new Background(myBIW));
-      //  memberZone.getChildren().add(helloTF);
-     //   helloTF.setTranslateY(-100);
-        memberZone.getChildren().add(name);
+        memberZone.setBackground(new Background(myBIW));      
+        name.setMaxWidth(400);
         name.setTranslateY(-50);
-        name.setTranslateX(440);
+        memberZone.getChildren().add(name);
+        password.setMaxWidth(400);
         memberZone.getChildren().add(password);
-        password.setTranslateY(0);
-        password.setTranslateX(440);
-
         memberZone.getChildren().add(getBack);
         getBack.setTranslateY(100);
         getBack.setTranslateX(-100);
---
+++ src/Requests/AcountValid.java
@@ -0,0 +1,10 @@
+package Requests;
+
+public class AcountValid {
+	public String username;
+    public String password;
+    public AcountValid(String _username,String _password){
+    	this.username = _username;
+    	this.password = _password;
+    }
+}
