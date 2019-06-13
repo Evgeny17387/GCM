f3e61b50d528c981e4cace776ef0b9d154d7aa31
unknown <evgeny17387@gmail.com>
2019-06-13 12:24:35 +0300

    19

--- src/GUI/SearchMapView.java
+++ src/GUI/SearchMapView.java
@@ -27,7 +27,11 @@ import javafx.stage.Stage;
 
 public class SearchMapView extends BaseView {
 
-    static TextField searchTF = new TextField("Type map to search");
+    static TextField searchTF;
+
+    static CheckBox Search_by_city;
+    static CheckBox Search_by_inplace ;
+    static CheckBox Search_by_general_description;
 
 	String request_string;
 
@@ -35,17 +39,25 @@ public class SearchMapView extends BaseView {
 
 		super(aChat);
 
+		// Init
+		
         BackgroundImage myBIc = new BackgroundImage(new Image("Images\\catalog_up.png"), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-        
-        CheckBox Search_by_city= new CheckBox("Search by city");
-        CheckBox Search_by_inplace= new CheckBox("Search by interested place");
-        CheckBox Search_by_general_description= new CheckBox("Search by general description");
+
+        searchTF = new TextField("Type map to search");
+
+        Search_by_city = new CheckBox("Search by city");
+        Search_by_inplace= new CheckBox("Search by interested place");
+        Search_by_general_description = new CheckBox("Search by general description");
+
         Search_by_city.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
         Search_by_inplace.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
         Search_by_general_description.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
         searchTF.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
 
-        Button search_btn=new Button("Search");
+        Button search_btn = new Button("Search");
+
+		// OnClick
+
         search_btn.setOnAction(e->{
 
         	if((Search_by_city.isSelected()&&Search_by_inplace.isSelected())||(Search_by_city.isSelected()&&Search_by_general_description.isSelected())||Search_by_inplace.isSelected()&&Search_by_general_description.isSelected())
@@ -85,13 +97,18 @@ public class SearchMapView extends BaseView {
     		
         });
 
+		// UI
+
         searchTF.setMaxWidth(300);
+
         searchTF.setTranslateY(-100);
         Search_by_city.setTranslateY(-250);
         Search_by_city.setTranslateX(-63);
         Search_by_inplace.setTranslateY(-200);
         Search_by_general_description.setTranslateY(-150);
 
+        // Scene
+        
         StackPane guestZone = new StackPane();
         guestZone.setBackground(new Background(myBIc));
         guestZone.getChildren().addAll(searchTF, search_btn, goBack, Search_by_city, Search_by_inplace, Search_by_general_description);
@@ -103,6 +120,10 @@ public class SearchMapView extends BaseView {
 	public static void refreshScene() {
 
 	     searchTF.setText("Type map to search");
+	     
+	     Search_by_city.setSelected(false);
+	     Search_by_inplace.setSelected(false);
+	     Search_by_general_description.setSelected(false);
 
 	}
 
